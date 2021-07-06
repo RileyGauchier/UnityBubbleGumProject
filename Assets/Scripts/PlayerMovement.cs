3f84de7cef16928aeb6493bb64d6ec4f54ba93e8
Riley <rileygauchier@gmail.com>
2021-07-05 22:33:14 -0600

    re-import library

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,46 +4,47 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] float _moveSpeed = 5f, _jumpForce = 10f;
-
+    [SerializeField] LayerMask _groundLayer = default;
+    [SerializeField] float _moveSpeed = 5f, _jumpForce = 1f;
+    float _xMove,_zMove;
     Rigidbody _rb;
+    CapsuleCollider _cc;
 
     private void Awake()
     {
         _rb = GetComponent<Rigidbody>();
+        _cc = GetComponentInChildren<CapsuleCollider>();
 
     }
 
     private void FixedUpdate()
     {
         PlayerCharacterWalkInputs();
+        PlayerCharacterJump();
     }
 
     void PlayerCharacterWalkInputs()
     {
-        if(Input.GetKey(KeyCode.W))
-        {
-            _rb.AddForce(_moveSpeed * Vector3.forward);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            _rb.AddForce(_moveSpeed * -Vector3.forward);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            _rb.AddForce(_moveSpeed * Vector3.right);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            _rb.AddForce(_moveSpeed * -Vector3.right);
-        }
+        _xMove = Input.GetAxisRaw("Horizontal");
+        _zMove = Input.GetAxisRaw("Vertical");
+
+        _rb.velocity = new Vector3(_xMove, _rb.velocity.y, _zMove) * _moveSpeed;
     }
 
     void PlayerCharacterJump()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Space) && IsGrounded())
         {
-            
+            _rb.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
         }
+    }
+
+    bool IsGrounded()
+    {
+        return Physics.CheckCapsule(_cc.bounds.center, new Vector3(_cc.bounds.center.x
+            , _cc.bounds.min.y,
+            _cc.bounds.center.z),
+            _cc.radius * 0.9f,
+            _groundLayer);
     }
 }
